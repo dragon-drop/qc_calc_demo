917cf7824ca989e181bb5056b55230bb27613fba
adamdawkins <adam@dragondrop.uk>
2024-09-12 13:17:34 +0100

    Working prototype

--- js/app.js
+++ js/app.js
@@ -1,102 +1,87 @@
 (function (window) {
   "use strict";
-  const { div, h1, h2, input, label, section } = van.tags;
+  const { div, form, h1, h2, input, label, p, section } = van.tags;
+  let programs = [
+    {
+      id: 1,
+      apr: 11.9,
+      term: 120,
+      monthly_payment_factor: 0.014347,
+      fee: 0,
+    },
+    {
+      id: 2,
+      apr: 11.9,
+      term: 144,
+      monthly_payment_factor: 0.013134,
+      fee: 0,
+    },
+    {
+      id: 3,
+      apr: 11.9,
+      term: 180,
+      monthly_payment_factor: 0.012001,
+      fee: 0,
+    },
+    {
+      id: 4,
+      apr: 9.9,
+      term: 120,
+      monthly_payment_factor: 0.01322,
+      fee: 0.06,
+    },
+    {
+      id: 5,
+      apr: 9.9,
+      term: 144,
+      monthly_payment_factor: 0.012,
+      fee: 0.06,
+    },
+    {
+      id: 6,
+      apr: 9.9,
+      term: 180,
+      monthly_payment_factor: 0.0107,
+      fee: 0.06,
+    },
+    {
+      id: 7,
+      apr: 7.9,
+      term: 120,
+      monthly_payment_factor: 0.0121,
+      fee: 0.1,
+    },
+    {
+      id: 8,
+      apr: 7.9,
+      term: 144,
+      monthly_payment_factor: 0.0108,
+      fee: 0.11,
+    },
+    {
+      id: 9,
+      apr: 7.9,
+      term: 180,
+      monthly_payment_factor: 0.00955,
+      fee: 0.12,
+    },
+    {
+      id: 10,
+      apr: 0.0,
+      term: 36,
+      monthly_payment_factor: 0.027778,
+      fee: 0.15,
+    },
+  ];
+  const selectedProgram = van.state(1);
+  const contractFinance = van.state(0);
+  const contractCash = van.state(0);
+  const qcCeiling = van.state(0);
 
-  let state = {
-    programs: [
-      {
-        id: 1,
-        apr: 11.9,
-        term: 120,
-        monthly_payment_factor: 0.014347,
-        fee: 0,
-        selected: van.state(false),
-      },
-      {
-        id: 2,
-        apr: 11.9,
-        term: 144,
-        monthly_payment_factor: 0.013134,
-        fee: 0,
-        selected: van.state(false),
-      },
-      {
-        id: 3,
-        apr: 11.9,
-        term: 180,
-        monthly_payment_factor: 0.012001,
-        fee: 0,
-        selected: van.state(false),
-      },
-      {
-        id: 4,
-        apr: 9.9,
-        term: 120,
-        monthly_payment_factor: 0.01322,
-        fee: 0.06,
-        selected: van.state(false),
-      },
-      {
-        id: 5,
-        apr: 9.9,
-        term: 144,
-        monthly_payment_factor: 0.012,
-        fee: 0.06,
-        selected: van.state(false),
-      },
-      {
-        id: 6,
-        apr: 9.9,
-        term: 180,
-        monthly_payment_factor: 0.0107,
-        fee: 0.06,
-        selected: van.state(false),
-      },
-      {
-        id: 7,
-        apr: 7.9,
-        term: 120,
-        monthly_payment_factor: 0.0121,
-        fee: 0.1,
-        selected: van.state(false),
-      },
-      {
-        id: 8,
-        apr: 7.9,
-        term: 144,
-        monthly_payment_factor: 0.0108,
-        fee: 0.11,
-        selected: van.state(false),
-      },
-      {
-        id: 9,
-        apr: 7.9,
-        term: 180,
-        monthly_payment_factor: 0.00955,
-        fee: 0.12,
-        selected: van.state(false),
-      },
-      {
-        id: 10,
-        apr: 0.0,
-        term: 36,
-        monthly_payment_factor: 0.027778,
-        fee: 0.15,
-        selected: van.state(false),
-      },
-    ],
-  };
-
-  const selectProgram = (id) =>
-    (state = {
-      ...state,
-      programs: state.programs.map((program) =>
-        program.id == id ? { ...program, selected: van.state(true) } : program,
-      ),
-    });
+  const selectProgram = (id) => (selectedProgram.val = parseInt(id));
 
   const Program = (program) => {
-    const isSelected = van.derive(() => program.selected);
+    const isSelected = van.derive(() => selectProgram == program.id);
     const id = `program_${program.id}`;
     return div(
       input({
@@ -110,12 +95,64 @@
     );
   };
 
+  const Form = () => {
+    return form(
+      div(
+        label("Qc Ceiling"),
+        input({
+          type: "number",
+          step: 0.01,
+          onchange: (e) => {
+            qcCeiling.val = parseFloat(e.target.value);
+          },
+        }),
+      ),
+      div(
+        label("Original Contract Cash"),
+        input({
+          type: "number",
+          step: 0.01,
+          onchange: (e) => (contractCash.val = parseFloat(e.target.value)),
+        }),
+      ),
+      div(
+        label("Original Contract Finance"),
+        input({
+          type: "number",
+          step: 0.01,
+          onchange: (e) => (contractFinance.val = parseFloat(e.target.value)),
+        }),
+      ),
+    );
+  };
+
+  const Calculations = () => {
+    const amountFeeAppliesTo = van.derive(
+      () => qcCeiling.val + contractFinance.val,
+    );
+    const fee = van.derive(
+      () => programs.find((p) => p.id === selectedProgram.val).fee,
+    );
+
+    const displayFee = van.derive(() => fee.val * 100);
+    const additionalCharge = van.derive(() => amountFeeAppliesTo.val * fee.val);
+
+    const newCeiling = van.derive(() => qcCeiling.val + additionalCharge.val);
+    return div(
+      div("Amount to apply fee to:", amountFeeAppliesTo),
+      div("fee: ", displayFee, "%"),
+      div("additionalCharge", additionalCharge),
+      h1("The new QC ceiling is", newCeiling),
+    );
+  };
+
   const QcCalculator = () => {
-    console.log({ state });
     return section(
       h1("QC Calculator"),
       h2("Programs:"),
-      state.programs.map(Program),
+      programs.map(Program),
+      Form,
+      Calculations,
     );
   };
 
